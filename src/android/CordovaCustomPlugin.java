e5213c0f025c9fdb720f801b7246eadcef774c76
Ansel001 <stefano.scarrone@anseltechnology.it>
2020-11-18 15:27:39 +0100

    Aggiunto riferimento alla classe Operations

--- src/android/CordovaCustomPlugin.java
+++ src/android/CordovaCustomPlugin.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.example.operations.Operations;
+
 /**
  * This class echoes a string called from JavaScript.
  */
